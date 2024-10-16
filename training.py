f683800fa31390e2ebb875d3d99f290743c13e12
Juan Paz <2024subject@gmail.com>
2024-10-16 19:24:46 -0400

    epoch update

--- training.py
+++ training.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("yolo11n.yaml")  # build a new model from YAML
 
 # Train the model
-results = model.train(data="config.yaml", epochs=5)
+results = model.train(data="config.yaml", epochs=50)
