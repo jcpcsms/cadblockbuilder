800257971d10ce5d9df04cf09bbc16b4c5ac2005
Juan Paz <2024subject@gmail.com>
2024-10-17 23:57:14 -0400

    Hyperparameters adjusted
    Due to the predictor module not recognizing any object we have adjusted hyperparameters to improve learning metrics.

--- training.py
+++ training.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("yolo11n.yaml")  # build a new model from YAML
 
 # Train the model
-results = model.train(data="config.yaml", epochs=50)
+results = model.train(data="config.yaml", epochs=50, optimize=True, imgsz=1024, dynamic=True, weight_decay=0.0001)
