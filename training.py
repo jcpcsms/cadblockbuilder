dcef12e546ef4fa8b92cfd833a1c8d50a285883c
Juan Paz <2024subject@gmail.com>
2024-10-18 21:52:17 -0400

    Successfully predicted first object

--- training.py
+++ training.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolo11n.yaml")  # build a new model from YAML
+model = YOLO("YOLO11m.yaml")  # build a new model from YAML
 
 # Train the model
-results = model.train(data="config.yaml", epochs=50, optimize=True, imgsz=1024, dynamic=True, weight_decay=0.0001)
+results = model.train(data="config0.yaml", epochs=50, optimize=True, imgsz=640, dynamic=True, weight_decay=0.0001, patience=15)
